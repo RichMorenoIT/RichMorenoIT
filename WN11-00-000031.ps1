ff6f2ac56304beb0f44b41a74b61c0d46b705ec7
RichMorenoIT <richmorenoit@gmail.com>
2026-05-23 23:06:51 -0700

    Update WN11-00-000031.ps1

--- WN11-00-000031.ps1
+++ WN11-00-000031.ps1
@@ -1,7 +1,7 @@
 
  <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures Windows 11 systems must use a BitLocker PIN for pre-boot authentication.
 
 .NOTES
     Author          : Ricardo Moreno
